f69d83aa5121b35af3b1b9eaac7a55174b0f7026
Jacinta Calixte <jacinta246@yahoo.com>
2020-06-12 00:09:20 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   count = 2
    new_array = []
    while count < array.length do
-     new_array.push(array[count].to_s)
+     new_array.push(array[count])
      count += 1
    end
    new_array
